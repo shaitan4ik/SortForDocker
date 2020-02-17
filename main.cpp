e5d0128bb998701b65b91669f23a15a09bef1040
shkEEpEr <v.d.grishakov@gmail.com>
2020-02-17 17:50:24 +0300

    Intermediate commit at work
     Changes to be committed:
    	modified:   .gitignore
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -20,11 +20,7 @@ vecInt creatRandomArray(int coutnOfNumber) {
     *it = i;
     it++;
   }
-  for (auto m : resultSequence) {
-    std::cout << m << std::endl;
-  }
   // Data randomization
-
   for (int i = 0; i < coutnOfNumber - 1; i++) {
 
     std::uniform_int_distribution<> uid(i, coutnOfNumber - 1);
@@ -34,16 +30,40 @@ vecInt creatRandomArray(int coutnOfNumber) {
   return resultSequence;
 }
 
-int main(int argc, char *argv[]) {
+void insertSort(vecInt &input) {
 
+  for (int i = 1; i < input.size(); i++) {
+    for (int j = i; j > 0 && input[j - 1] > input[j]; j--) {
+      std::swap(input[j - 1], input[j]);
+    }
+  }
+}
+
+void selectionSort(vecInt &input) {
+  int minElementIndex = 0;
+  for (int i = 0; i < input.size(); i++) {
+    minElementIndex = i;
+    for (int j = i; j < input.size(); j++) {
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  // Start timeCounter
   time_point<high_resolution_clock> star, end;
   star = high_resolution_clock::now();
-
-  std::cout << "Hello people" << std::endl;
+  //---------------------
+  // Main block
   vecInt rArray = creatRandomArray(1000);
   for (auto m : rArray) {
-    std::cout << m << std::endl;
+    cout << m << endl;
   }
+  insertSort(rArray);
+  for (auto m : rArray) {
+    cout << m << endl;
+  }
+  // End block
+  // End timeCounter
   end = high_resolution_clock::now();
 
   int elapsedTime = duration_cast<milliseconds>(end - star).count();
@@ -51,6 +71,6 @@ int main(int argc, char *argv[]) {
 
   cout << "Время завершения выполнения: " << ctime(&endTime)
        << "Время выполнения: " << elapsedTime << " milliseconds" << endl;
-
+  //----------------------
   return 0;
 }
