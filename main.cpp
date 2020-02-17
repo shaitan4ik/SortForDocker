81bd5fdfa8f17cf6f984378ade255c6248b092bd
shkEEpEr <v.d.grishakov@gmail.com>
2020-02-17 16:45:06 +0300

    Change main.cpp and .gitignore
    Main.cpp:
    	- add timecounte
    	- finish work on creatRandom Array
    .gitignore:
    	- add in block exutables "main"
    	- add in block VScode setting's folder template .VScode

--- main.cpp
+++ main.cpp
@@ -1,42 +1,56 @@
-#include <iostream> 
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <random>
 #include <string>
 #include <vector>
-#include <random>
-#include <ctime>
-using vecInt = std::vector<int>; 
 
-vecInt creatRandomArray(int coutnOfNumber){
+using vecInt = std::vector<int>;
+using std::cout;
+using namespace std::chrono;
+using std::endl;
 
-vecInt resultSequence(coutnOfNumber);
-std::mt19937 gen(std::time(0)); 
-//Creat init sequence
-vecInt::iterator it = resultSequence.begin();
-for(int i; i < coutnOfNumber; i++){
-    *it = i; 
-    it++; 
+vecInt creatRandomArray(int coutnOfNumber) {
+
+  vecInt resultSequence(coutnOfNumber);
+  std::mt19937 gen(std::time(0));
+  // Creat init sequence
+  vecInt::iterator it = resultSequence.begin();
+  for (int i; i < coutnOfNumber; i++) {
+    *it = i;
+    it++;
+  }
+  for (auto m : resultSequence) {
+    std::cout << m << std::endl;
+  }
+  // Data randomization
+
+  for (int i = 0; i < coutnOfNumber - 1; i++) {
+
+    std::uniform_int_distribution<> uid(i, coutnOfNumber - 1);
+    std::swap(resultSequence[i], resultSequence[uid(gen)]);
+  }
+
+  return resultSequence;
 }
-for(auto m: resultSequence){
-    std::cout << m << std::endl; 
-}
-//Data randomization 
 
-for(int i=0; i < coutnOfNumber-1; i++){ 
+int main(int argc, char *argv[]) {
 
-std::uniform_int_distribution<>uid(i,coutnOfNumber-1);
-std::swap(resultSequence[i], resultSequence[uid(gen)]);
+  time_point<high_resolution_clock> star, end;
+  star = high_resolution_clock::now();
 
-}
+  std::cout << "Hello people" << std::endl;
+  vecInt rArray = creatRandomArray(1000);
+  for (auto m : rArray) {
+    std::cout << m << std::endl;
+  }
+  end = high_resolution_clock::now();
 
+  int elapsedTime = duration_cast<milliseconds>(end - star).count();
+  std::time_t endTime = high_resolution_clock::to_time_t(end);
 
-return resultSequence; 
+  cout << "Время завершения выполнения: " << ctime(&endTime)
+       << "Время выполнения: " << elapsedTime << " milliseconds" << endl;
 
-}
-
-int main(int argc, char *argv[]){
-
-std::cout << "Hello people" << std::endl;  
-
-
-    return 0; 
-
+  return 0;
 }
